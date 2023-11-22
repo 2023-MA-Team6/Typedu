489db274bb12adf5f59cc09b3aab4056868c9737
qh0614dd <qh0614dd@knu.ac.kr>
2023-11-23 01:14:15 +0900

    Pending KeyBoard..

--- app/src/main/java/com/example/typedu/KeyboardActivity.kt
+++ app/src/main/java/com/example/typedu/KeyboardActivity.kt
@@ -1,15 +1,18 @@
 package com.example.typedu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.typedu.databinding.ActivityKeyboardBinding
 
 class KeyboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityKeyboardBinding
+    private lateinit var wordList: List<String>
+    private var currentWordIndex = 0
     private var typedCharCount = 0
     private var currentTypingSpeed = 0
     private var isTyping = false
@@ -19,7 +22,11 @@ class KeyboardActivity : AppCompatActivity() {
         binding = ActivityKeyboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // EditText에 TextWatcher 추가
+        wordList = resources.getStringArray(R.array.random_korean_words).toList()
+
+        // 초기화면에 첫 번째 단어 표시
+        showNextWord()
+
         binding.currentWordText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // 이전 텍스트 변경 이벤트
@@ -30,18 +37,27 @@ class KeyboardActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // 텍스트 변경 후 이벤트
-                val currentWord = binding.WordView.getChildAt(2) as TextView
+                val currentWordView = binding.WordView.getChildAt(2) as? TextView
+                val currentWord = currentWordView?.text.toString()
                 val currentWordText = binding.currentWordText.text.toString()
-
-                if (currentWord.text == currentWordText) {
-                    // 현재 타이핑 중인 단어가 일치하면 typedChars의 text를 +1
+                if (currentWord == currentWordText) {
                     typedCharCount++
                     binding.typedChars.text = typedCharCount.toString()
+
+                    // 단어 일치 시 다음 단어로 이동하고 텍스트를 왼쪽으로 이동
+                    shiftTextLeft()
+                    showNextWord()
+
+                    // 새로운 값은 3번째 TextView에 저장
+                    /*binding.WordView.getChildAt(2)?.let {
+                        if (it is TextView) {
+                            it.text = currentWordText
+                        }
+                    }*/
+
                 }
 
                 if (!isTyping) {
-                    // 타이핑 중이 아니라면 타이핑 중 상태로 변경하고 타수 계산 시작
                     isTyping = true
                     startTypingSpeedCalculator()
                 }
@@ -54,16 +70,38 @@ class KeyboardActivity : AppCompatActivity() {
 
         val typingSpeedCalculator = object : Runnable {
             override fun run() {
-                // 1초마다 현재 타수 계산 및 업데이트
-                currentTypingSpeed = (typedCharCount * 60) / 5 // 예시로 5초 기준으로 타수 계산
+                currentTypingSpeed = (typedCharCount * 60) / 5
                 binding.currentTyping.text = currentTypingSpeed.toString()
 
-                // 다음 1초 후에 다시 실행
                 handler.postDelayed(this, 1000)
             }
         }
 
-        // 처음에도 1초 후에 실행
         handler.postDelayed(typingSpeedCalculator, 1000)
     }
+
+    private fun showNextWord() {
+        val currentWordTextView = binding.WordView.getChildAt(2) as TextView
+
+        if (currentWordIndex < wordList.size) {
+            currentWordTextView.text = wordList[currentWordIndex]
+            currentWordIndex++
+        } else {
+            currentWordIndex = 0
+            currentWordTextView.text = wordList[currentWordIndex]
+        }
+    }
+    private fun shiftTextLeft() {
+        // 1번째 TextView 초기화
+        (binding.WordView.getChildAt(0) as TextView)?.text = ""
+
+        // 2번째와 3번째 TextView의 값 이동
+        for (i in 0..1) {
+            val currentWordTextView = binding.WordView.getChildAt(i + 1) as? TextView
+            val previousWordTextView = binding.WordView.getChildAt(i) as? TextView
+
+            previousWordTextView?.text = currentWordTextView?.text
+        }
+    }
+
 }
