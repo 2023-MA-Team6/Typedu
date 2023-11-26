3b54ad3c7e4bbfd3b279cc3deeb23e76f5dc9856
qh0614dd <qh0614dd@knu.ac.kr>
2023-11-26 16:46:09 +0900

    ShortStatement Finished

--- app/src/main/java/com/example/typedu/ShortParagraphActivity.kt
+++ app/src/main/java/com/example/typedu/ShortParagraphActivity.kt
@@ -1,19 +1,35 @@
 package com.example.typedu
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.typedu.databinding.ActivityShortParagraphBinding
+import kotlinx.coroutines.*
 
 class ShortParagraphActivity : AppCompatActivity() {
     private lateinit var binding: ActivityShortParagraphBinding
     private var currentSentenceIndex = 0
-    private val MAX_SENTENCES = 20  // 표시할 최대 문장 개수
+    private var typedCharCount = 0
+    private var correctCharCount = 0
+
+    private var totalTypedCount = 0
+    private var totalCorrectCount = 0
+    private var calcTypingSpeed = 0
+
+    private var isTyping = false
+    private val handler = Handler()
+    private var elapsedTimeInSeconds = 0
+
+    private var lastTyping = 0
+    private var highestTypingSpeed = 0
+    private var MAX_ITEM_COUNT = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +44,29 @@ class ShortParagraphActivity : AppCompatActivity() {
 
         // 다음 문장 표시
         showNextSentence()
+
+        // 타이머 시작
+        startTimer()
+
+        // 타수 및 정확도 갱신
+        startTypingSpeedCalculator()
+    }
+
+    private fun startTimer() {
+        GlobalScope.launch(Dispatchers.Main) {
+            while (isActive) {
+                delay(1000)
+                elapsedTimeInSeconds++
+                updateTimer()
+            }
+        }
+    }
+
+    private fun updateTimer() {
+        val minutes = elapsedTimeInSeconds / 60
+        val seconds = elapsedTimeInSeconds % 60
+        val timeString = String.format("%02d:%02d", minutes, seconds)
+        binding.time.text = timeString
     }
 
     private fun setupEditText() {
@@ -42,8 +81,17 @@ class ShortParagraphActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // 사용자의 입력이 변경될 때 처리하는 부분
-                // 필요한 기능을 여기에 추가하세요.
+                val currentSentence = binding.currentStatement.text.toString()
+                val currentEditText = binding.currentWordText.text.toString()
+
+                // 글자 일치 여부 확인
+                checkTypingAccuracy(currentSentence, currentEditText)
+
+
+                if (!isTyping) {
+                    isTyping = true
+                    startTypingSpeedCalculator()
+                }
             }
         })
     }
@@ -58,6 +106,7 @@ class ShortParagraphActivity : AppCompatActivity() {
             textView.textSize = 20f
             scrollView.addView(textView)
         }
+        MAX_ITEM_COUNT = scrollView.childCount
     }
 
     // 다음 문장 표시
@@ -65,15 +114,99 @@ class ShortParagraphActivity : AppCompatActivity() {
         val scrollView: LinearLayout = findViewById(R.id.contentScrollView)
         val currentStatementView: TextView = findViewById(R.id.currentStatement)
 
-        if (currentSentenceIndex < scrollView.childCount) {
-            val currentSentenceTextView = scrollView.getChildAt(currentSentenceIndex) as? TextView
+        if (currentSentenceIndex < MAX_ITEM_COUNT) {
+            val currentSentenceTextView = scrollView.getChildAt(0) as? TextView
             val currentSentence = currentSentenceTextView?.text.toString()
             currentStatementView.text = currentSentence
-            currentSentenceIndex++
-
             scrollView.removeViewAt(0)
+            currentSentenceIndex++
         } else {
             // 추가 작업이 필요한 경우 여기에 코드를 추가하세요.
+        }
+    }
+
+    private fun startTypingSpeedCalculator() {
+        GlobalScope.launch(Dispatchers.Main) {
+            while (isActive) {
+                delay(1000)
+                val currentTypingSpeed = calculateTypingSpeedPerSecond(calcTypingSpeed, elapsedTimeInSeconds)
+                binding.currentTyping.text = currentTypingSpeed.toString()
+
+                // 최고 타수 갱신
+                if (currentTypingSpeed > highestTypingSpeed) {
+                    highestTypingSpeed = currentTypingSpeed
+                    binding.highestTyping.text = highestTypingSpeed.toString()
+                }
+
+                // 정확도 갱신
+                binding.accuracy.text = calculateAccuracy().toString() + "%"
+            }
+        }
+    }
+
+    private fun calculateTypingSpeedPerSecond(typedCharCount: Int, elapsedTimeInSeconds: Int): Int {
+        return if (elapsedTimeInSeconds > 0) {
+            Log.d("bowon", "$typedCharCount , $elapsedTimeInSeconds")
+            typedCharCount * 60 / elapsedTimeInSeconds
+        } else {
+            0
+        }
+    }
+
+
+    var index: Int = 0
+    private fun checkTypingAccuracy(sentence: String, userText: String) {
+        if(sentence == userText) {
+            index = 0
+            totalTypedCount += typedCharCount
+            totalCorrectCount += correctCharCount
+            typedCharCount = 0
+            correctCharCount = 0
+            showNextSentence()
+            binding.currentWordText.setText("")
+            calcTypingSpeed++
+        } else if(sentence.length <= userText.length) {
+            if(sentence[index + 1] == userText[index + 1])
+                correctCharCount++
+            typedCharCount++
+            index = 0
+
+            totalTypedCount += typedCharCount
+            totalCorrectCount += correctCharCount
+            typedCharCount = 0
+            correctCharCount = 0
+            showNextSentence()
+            binding.currentWordText.setText("")
+            calcTypingSpeed++
+        } else {
+            if(index < userText.length -1) {
+
+                if(sentence[index] == userText[index]) {
+                    correctCharCount++
+                    typedCharCount++
+                } else {
+                    typedCharCount++
+                }
+                index++
+                calcTypingSpeed++
+            } else if(index > userText.length - 1 && userText.length - 1 >= 0) {
+                typedCharCount--
+                index--
+
+                correctCharCount = 0
+                for(i in 0 until index) {
+                    if(sentence[i] == userText[i])
+                        correctCharCount++
+                }
+            }
+        }
+    }
+
+    private fun calculateAccuracy(): Int {
+        return if (totalTypedCount > 0) {
+            (totalCorrectCount.toDouble() / totalTypedCount.toDouble() * 100).toInt()
+        } else {
+            0
         }
     }
 }
