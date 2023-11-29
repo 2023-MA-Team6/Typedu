15f050ff3678bbc34e3e7ebdd1943c6ed8d1b2a0
JYN523 <87629686+JYN523@users.noreply.github.com>
2023-11-29 17:29:06 +0900

    대문자 파일명 수정

--- app/src/main/java/com/example/typedu/setArticleDialog.kt
+++ app/src/main/java/com/example/typedu/setArticleDialog.kt
@@ -2,22 +2,29 @@ package com.example.typedu
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.widget.Toast
+import com.example.typedu.databinding.DialogSetArticleBinding
 import com.example.typedu.databinding.DialogSetTargetBinding
+import java.util.Objects
 
 class setArticleDialog(context:Context) : Dialog(context) {
-//    private lateinit var binding : DialogSetTargetBinding
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = DialogSetTargetBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        setCanceledOnTouchOutside(true)
-//        setCancelable(true)
-//
-//        binding.cancelBtn.setOnClickListener {
-//            dismiss()
-//        }
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DialogSetArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Objects.requireNonNull(window).setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        binding.nextBtn.setOnClickListener {
+            Toast.makeText(context, "click next Button", Toast.LENGTH_SHORT).show();
+        }
+
+        binding.cancelBtn.setOnClickListener {
+            Toast.makeText(context, "click cancel Button", Toast.LENGTH_SHORT).show();
+            dismiss()
+        }
+    }
 }
